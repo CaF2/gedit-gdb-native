0f7d5cc8dfe30e8638e75a3d0d26ffec52b9941f
Florian Evaldsson <florian.evaldsson@telia.com>
2026-01-21 14:45:51 +0100

    made it more functional

--- gedit-gdb.h
+++ gedit-gdb.h
@@ -10,6 +10,14 @@ G_BEGIN_DECLS
 #define GEDIT_TYPE_GDB_PLUGIN (gedit_gdb_plugin_get_type())
 G_DECLARE_FINAL_TYPE(GeditGdbPlugin, gedit_gdb_plugin, GEDIT, GDB_PLUGIN, GObject)
 
+typedef enum 
+{
+	GEDIT_GDB_CONNECTION_STATUS_NOT_CONNECTED,
+	GEDIT_GDB_CONNECTION_STATUS_CONNECTING,
+	GEDIT_GDB_CONNECTION_STATUS_CONNECTED,
+	GEDIT_GDB_CONNECTION_STATUS_LENGTH
+}GeditGdbConnectionStatus;
+
 struct _GeditGdbPlugin {
     GObject parent_instance;
     GeditWindow *window; // Nu känd via gedit-window.h
@@ -19,6 +27,8 @@ struct _GeditGdbPlugin {
     gint gdb_stdin;
     gint gdb_stdout;
     GIOChannel *gdb_out_chan;
+    
+    GeditGdbConnectionStatus connected;
 };
 
 G_END_DECLS
